d9e56b0ab1a4430034978baf2f6d7244a88154a2
jmarc <jmarc@169.232.156.85>
2024-02-24 14:12:23 -0800

    refactor region coloring

---
+++ GuiElements/RegionColor.m
@@ -0,0 +1,9 @@
+classdef RegionColor
+    properties (Constant)
+        unprocessedColor = [0, 0.447, 0.741]; % default rectangle color
+        queueColor = "red";
+        workingColor = "yellow";
+        finishedColor = "green";
+    end
+end
+
