87b5f17100bb72892ba7b67927f0f95987cce3b5
jmarc <jmarc@169.232.156.85>
2024-02-26 20:36:08 -0800

    compress/expand any region shape

--- GuiElements/RegionAdjuster/RegionExpander/RegionExpander.m
+++ GuiElements/RegionAdjuster/RegionExpander/RegionExpander.m
@@ -81,26 +81,104 @@ end
 
 
 function expandUp(region)
-position = get(region, "Position");
-position(2) = position(2) - 1;
-position(4) = position(4) + 1;
-set(region, "Position", position);
+if RegionType.isRectangle(region)
+    rectangleUp(region);
+elseif RegionType.isEllipse(region)
+    ellipseUp(region);
+elseif RegionType.hasVertexPosition(region)
+    vertexUp(region);
 end
+end
+
 function expandDown(region)
-position = get(region, "Position");
-position(4) = position(4) + 1;
-set(region, "Position", position);
+if RegionType.isRectangle(region)
+    rectangleDown(region);
+elseif RegionType.isEllipse(region)
+    ellipseDown(region);
+elseif RegionType.hasVertexPosition(region)
+    vertexDown(region);
 end
+end
+
 function expandLeft(region)
-position = get(region, "Position");
-position(1) = position(1) - 1;
-position(3) = position(3) + 1;
-set(region, "Position", position);
+if RegionType.isRectangle(region)
+    rectangleLeft(region);
+elseif RegionType.isEllipse(region)
+    ellipseLeft(region);
+elseif RegionType.hasVertexPosition(region)
+    vertexLeft(region);
 end
+end
+
 function expandRight(region)
-position = get(region, "Position");
-position(3) = position(3) + 1;
-set(region, "Position", position);
+if RegionType.isRectangle(region)
+    rectangleRight(region);
+elseif RegionType.isEllipse(region)
+    ellipseRight(region);
+elseif RegionType.hasVertexPosition(region)
+    vertexRight(region);
+end
+end
+
+
+
+
+
+function rectangleUp(rect)
+rect.Position(2) = rect.Position(2) - 1;
+rect.Position(4) = rect.Position(4) + 1;
+end
+function rectangleDown(rect)
+rect.Position(4) = rect.Position(4) + 1;
+end
+function rectangleLeft(rect)
+rect.Position(1) = rect.Position(1) - 1;
+rect.Position(3) = rect.Position(3) + 1;
+end
+function rectangleRight(rect)
+rect.Position(3) = rect.Position(3) + 1;
+end
+
+function ellipseUp(ellipse)
+ellipse.SemiAxes(2) = ellipse.SemiAxes(2) + 1;
+end
+function ellipseDown(ellipse)
+ellipse.SemiAxes(2) = ellipse.SemiAxes(2) + 1;
+end
+function ellipseLeft(ellipse)
+ellipse.SemiAxes(1) = ellipse.SemiAxes(1) + 1;
+end
+function ellipseRight(ellipse)
+ellipse.SemiAxes(1) = ellipse.SemiAxes(1) + 1;
+end
+
+function vertexUp(polygon)
+y = polygon.Position(:, 2);
+polygonHeight = range(y);
+ymax = max(y);
+scaleFactor = (polygonHeight + 1) / polygonHeight;
+polygon.Position(:, 2) = ymax + scaleFactor * (y - ymax);
+end
+function vertexDown(polygon)
+y = polygon.Position(:, 2);
+polygonHeight = range(y);
+ymin = min(y);
+scaleFactor = (polygonHeight + 1) / polygonHeight;
+polygon.Position(:, 2) = ymin + scaleFactor * (y - ymin);
+end
+function vertexLeft(polygon)
+x = polygon.Position(:, 1);
+polygonHeight = range(x);
+xmax = max(x);
+scaleFactor = (polygonHeight + 1) / polygonHeight;
+polygon.Position(:, 1) = xmax + scaleFactor * (x - xmax);
+end
+function vertexRight(polygon)
+x = polygon.Position(:, 1);
+polygonHeight = range(x);
+xmin = min(x);
+scaleFactor = (polygonHeight + 1) / polygonHeight;
+polygon.Position(:, 1) = xmin + scaleFactor * (x - xmin);
 end
 
 function expandUpLeft(region)
