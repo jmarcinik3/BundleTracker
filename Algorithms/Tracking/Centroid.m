7451142484ae4eedd2167054ba3554e516ef8b44
jmarc <jmarc@169.232.156.85>
2024-03-09 03:07:15 -0800

    minor speed improvement

--- Algorithms/Tracking/Centroid.m
+++ Algorithms/Tracking/Centroid.m
@@ -19,10 +19,10 @@ classdef Centroid
             wx = weights .* x;
             wy = weights .* y;
 
-            obj.rows = rows;
-            obj.columns = columns;
             obj.wxx = wx .* x;
             obj.wyy = wy .* y;
+            obj.rows = rows;
+            obj.columns = columns;
             obj.weights = weights;
             obj.wx = wx;
             obj.wy = wy;
@@ -44,36 +44,27 @@ classdef Centroid
         function [xerr, yerr] = bootstrapError(obj, count)
             xy = zeros(4, count);
 
+            wx = obj.wx;
+            wy = obj.wy;
+            wxx = obj.wxx;
+            wyy = obj.wyy;
+
+            rows = obj.rows;
+            columns = obj.columns;
+            weights = obj.weights;
+
             for index = 1:count
-                [xmean, ymean, xstd, ystd] = obj.bootstrap();
+                [mask, area] = generateWeights(weights, rows, columns);
+                [xmean, ymean, xstd, ystd] = centroidFromWXY( ...
+                    area, wx(mask), wy(mask), wxx(mask), wyy(mask) ...
+                    );
                 xy(:, index) = [xmean, ymean, xstd, ystd];
             end
-            
+
             xymeans = xy(1:2, :);
             xystds = xy(3:4, :);
             xerr = std(xymeans(1), 1 ./ (xystds(1).^2));
             yerr = std(xymeans(2), 1 ./ (xystds(2).^2));
-        end
-
-        function [randomMask, newArea] = generateWeights(obj)
-            rows = obj.rows;
-            columns = obj.columns;
-            weights = obj.weights;
-            newArea = 0;
-
-            while newArea == 0
-                randomMask = rand(rows, columns) > weights;
-                weights(~randomMask) = 0;
-                newArea = sum(weights, "all");
-            end
-        end
-
-        function [xmean, ymean, xstd, ystd] = bootstrap(obj)
-            [mask, area] = obj.generateWeights();
-            xmean = sum(obj.wx(mask), "all") / area;
-            ymean = sum(obj.wy(mask), "all") / area;
-            xstd = sqrt(sum(obj.wxx(mask), "all") / area - xmean^2);
-            ystd = sqrt(sum(obj.wyy(mask), "all") / area - ymean^2);
         end
     end
 end
@@ -89,4 +80,20 @@ function weights = calculateWeights(im)
 intensity = double(im);
 area = sum(intensity, "all");
 weights = intensity / area;
+end
+
+function [xmean, ymean, xstd, ystd] = centroidFromWXY(area, wx, wy, wxx, wyy)
+xmean = sum(wx, "all") / area;
+ymean = sum(wy, "all") / area;
+xstd = sqrt(sum(wxx, "all") / area - xmean^2);
+ystd = sqrt(sum(wyy, "all") / area - ymean^2);
+end
+
+function [randomMask, newArea] = generateWeights(weights, rows, columns)
+newArea = 0;
+while newArea == 0
+    randomMask = rand(rows, columns) > weights;
+    weights(~randomMask) = 0;
+    newArea = sum(weights, "all");
+end
 end
