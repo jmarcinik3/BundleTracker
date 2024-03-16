1929adb4a0ffe7bccde3a4364755780ab0e02fb7
jmarc <jmarc@169.232.156.85>
2024-03-16 15:50:49 -0700

    convert subsequence of TIFs to MJ2

--- Algorithms/Convert/convertToVideo.m
+++ Algorithms/Convert/convertToVideo.m
@@ -1,11 +1,19 @@
 function convertToVideo(filepaths, videoFilepath, varargin)
+p = inputParser;
+addOptional(p, "EveryNthFrame", 1);
+addOptional(p, "FrameRate", 60);
+parse(p, varargin{:});
+everyNthFrame = p.Results.EveryNthFrame;
+frameRate = p.Results.FrameRate;
+
 frameCount = numel(filepaths);
-progress = ProgressBar(frameCount, "Combining TIFs");
+frameNumbers = 1:everyNthFrame:frameCount;
+progress = ProgressBar(numel(frameNumbers), "Combining TIFs");
 videoWriter = VideoWriter(videoFilepath, "Archival");
-set(videoWriter, varargin{:});
+set(videoWriter, "FrameRate", frameRate);
 open(videoWriter);
 
-for index = 1:frameCount
+for index = frameNumbers
     im = imread(filepaths(index));
     writeVideo(videoWriter, im);
     count(progress);
