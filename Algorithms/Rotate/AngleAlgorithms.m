ccd89500986c8ed609741d053d2b50214c904a0e
jmarc <jmarc@169.232.156.85>
2024-03-31 17:29:18 -0700

    determine angle by k-means clustering

--- Algorithms/Rotate/AngleAlgorithms.m
+++ Algorithms/Rotate/AngleAlgorithms.m
@@ -1,41 +1,40 @@
 classdef AngleAlgorithms
     properties (Constant)
-        linearFit = "Linear Regression";
         ellipseFit = "Elliptical Regression";
-        keywords = [ ...
-            AngleAlgorithms.ellipseFit ...    
+        kmeansKeyword = "K-Means Clustering";
+        linearFit = "Linear Regression";
+        keywords = sort([ ...
+            AngleAlgorithms.ellipseFit, ...
+            AngleAlgorithms.kmeansKeyword, ...
             AngleAlgorithms.linearFit, ...
-            ];
+            ]);
     end
 
     methods (Static)
         function [angle, angleError, angleInfo] = byKeyword(x, y, keyword)
             switch keyword
-                case AngleAlgorithms.linearFit
-                    [angle, angleError, angleInfo] = AngleAlgorithms.byLinearFit(x, y);
                 case AngleAlgorithms.ellipseFit
                     [angle, angleError, angleInfo] = AngleAlgorithms.byEllipseFit(x, y);
+                case AngleAlgorithms.kmeansKeyword
+                    [angle, angleError, angleInfo] = AngleAlgorithms.byKMeansClustering(x, y);
+                case AngleAlgorithms.linearFit
+                    [angle, angleError, angleInfo] = AngleAlgorithms.byLinearFit(x, y);
             end
         end
 
+        function [angle, angleError, fitInfo] = byEllipseFit(x, y)
+            [angle, angleError, fitInfo] = byEllipseFit(x, y);
+        end
         function [angle, angleError, fitInfo] = byLinearFit(x, y)
             [angle, angleError, fitInfo] = byLinearFit(x, y);
         end
-        function [angle, angleError, fitInfo] = byEllipseFit(x, y)
-            [angle, angleError, fitInfo] = byEllipseFit(x, y);
+        function [angle, angleError, fitInfo] = byKMeansClustering(x, y)
+            [angle, angleError, fitInfo] = byKMeansClustering(x, y);
         end
     end
 end
 
 
-
-function [angle, angleError, fitInfo] = byLinearFit(x, y)
-fitInfo = fitlm(x, y);
-slope = fitInfo.Coefficients.Estimate(2);
-slopeError = fitInfo.Coefficients.SE(2);
-angle = atan(slope);
-angleError = slopeError / (1+slope^2);
-end
 
 function [angle, angleError, fitInfo] = byEllipseFit(x, y)
 xy = [x; y];
@@ -52,3 +51,28 @@ fitInfo = struct( ...
     "Angle", angle ...
     );
 end
+
+function [angle, angleError, fitInfo] = byKMeansClustering(x, y)
+xy = [x', y'];
+[labels, centers] = kmeans(xy, 2);
+xcenters = centers(:, 1);
+ycenters = centers(:, 2);
+deltaX = diff(xcenters);
+deltaY = diff(ycenters);
+angle = atan(deltaY / deltaX);
+angleError = Inf;
+fitInfo = struct( ...
+    "Labels", labels, ...
+    "Centers", centers ...
+    );
+end
+
+function [angle, angleError, fitInfo] = byLinearFit(x, y)
+fitInfo = fitlm(x, y);
+slope = fitInfo.Coefficients.Estimate(2);
+slopeError = fitInfo.Coefficients.SE(2);
+angle = atan(slope);
+angleError = slopeError / (1+slope^2);
+end
+
+
