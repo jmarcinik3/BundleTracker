e18a8c6311ded6fbb5f5db48dbacf58747d282bb
jmarcinik3 <jmarcinik3@gmail.com>
2024-05-01 19:11:30 -0700

    update region label upon delete

--- GuiElements/Region/deleteRegions.m
+++ GuiElements/Region/deleteRegions.m
@@ -1,8 +1,12 @@
 function deleteRegions(regions)
+ax = ancestor(regions, "axes");
 regionCount = numel(regions);
+
 for index = 1:regionCount
     region = regions(index);
     region.notify("DeletingROI");
     delete(region);
 end
+
+RegionUpdater.labels(ax);
 end
